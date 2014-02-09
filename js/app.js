619f8e3c774c1fe7d0735b94235e3880c41ad78f
Bhargav Golla <bhargav.golla@gmail.com>
2014-02-09 04:14:36 -0500

    Basic Typeahead

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
-var option = "";
-/*var courses = new Bloodhound({
-	datumTokenizer: function(d) { return d.tokens; },
-	queryTokenizer: Bloodhound.tokenizers.whitespace,
-	prefetch: './assets/courses.json'
-});
-courses.initialize();*/
 $(document).ready(function(){
+    var option = "";
+    var courses = new Bloodhound({
+	    datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.coursename+" "+d.type); },
+	    queryTokenizer: Bloodhound.tokenizers.whitespace,
+        prefetch: './assets/courses.json'
+    });
+    courses.initialize();
 	$('ul.navbar-nav>li').click(function(){
 		if ($('button.navbar-toggle').is(':visible')) {
 			$('div.navbar-collapse').collapse('hide');
 		}
 	});
-    /*$('#courses .typeahead').typeahead(null, {
+    $('input.typeahead').typeahead(null, {
         name: 'cpscCourses',
         displayKey: 'coursename',
         source: courses.ttAdapter()
-    });*/
+    });
 });
