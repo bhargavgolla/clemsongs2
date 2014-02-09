6e028c9e5c8582c50f151a1485a0832dc9cbb468
Bhargav Golla <bhargav.golla@gmail.com>
2014-02-09 03:15:01 -0500

    Rolling back Typeahead

--- js/app.js
+++ js/app.js
@@ -1,26 +1,19 @@
 var option = "";
-var courses = new Bloodhound({
+/*var courses = new Bloodhound({
 	datumTokenizer: function(d) { return d.tokens; },
 	queryTokenizer: Bloodhound.tokenizers.whitespace,
 	prefetch: './assets/courses.json'
 });
-courses.initialize();
+courses.initialize();*/
 $(document).ready(function(){
 	$('ul.navbar-nav>li').click(function(){
 		if ($('button.navbar-toggle').is(':visible')) {
 			$('div.navbar-collapse').collapse('hide');
 		}
 	});
-    $('#courses .typeahead').typeahead(null, {
+    /*$('#courses .typeahead').typeahead(null, {
         name: 'cpscCourses',
         displayKey: 'coursename',
-        source: courses.ttAdapter(),
-        templates: {
-            suggestion: Handlebars.compile([
-                '<p class="coreArea">{{type}}</p>',
-                '<p class="courseName">{{coursename}}</p>',
-                '<p class="courseDescription">{{description}}</p>'
-                ].join(''))
-        }
-    });
+        source: courses.ttAdapter()
+    });*/
 });
