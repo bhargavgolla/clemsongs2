3ab5b8746f635172144c73e22245b6c065ce430d
Bhargav Golla <bhargav.golla@gmail.com>
2014-02-09 03:09:30 -0500

    Adding Typeahead

--- js/app.js
+++ js/app.js
@@ -1,8 +1,26 @@
 var option = "";
+var courses = new Bloodhound({
+	datumTokenizer: function(d) { return d.tokens; },
+	queryTokenizer: Bloodhound.tokenizers.whitespace,
+	prefetch: './assets/courses.json'
+});
+courses.initialize();
 $(document).ready(function(){
 	$('ul.navbar-nav>li').click(function(){
 		if ($('button.navbar-toggle').is(':visible')) {
 			$('div.navbar-collapse').collapse('hide');
 		}
 	});
+    $('#courses .typeahead').typeahead(null, {
+        name: 'cpscCourses',
+        displayKey: 'coursename',
+        source: courses.ttAdapter(),
+        templates: {
+            suggestion: Handlebars.compile([
+                '<p class="coreArea">{{type}}</p>',
+                '<p class="courseName">{{coursename}}</p>',
+                '<p class="courseDescription">{{description}}</p>'
+                ].join(''))
+        }
+    });
 });
