54db90f8a7b6d0693d0e2e26e85fb99c621749ad
Bhargav Golla <bhargav.golla@gmail.com>
2014-02-09 02:29:21 -0500

    Changing brand name

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('a.navbar-brand,ul.navbar-nav>li').click(function(){
+	$('ul.navbar-nav>li').click(function(){
 		if ($('button.navbar-toggle').is(':visible')) {
 			$('div.navbar-collapse').collapse('hide');
 		}
