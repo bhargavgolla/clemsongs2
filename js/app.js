825ff755c3d2b248677ab044e6f604a6d895bddb
Bhargav Golla <bhargav.golla@gmail.com>
2014-02-09 04:39:03 -0500

    Sending object details on select

--- js/app.js
+++ js/app.js
@@ -1,62 +1,40 @@
 $(document).ready(function(){
     var option = "";
     var courses = new Bloodhound({
-	    datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.coursename+" "+d.type); },
+	    datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.courseid+" "+d.coursename+" "+d.type); },
 	    queryTokenizer: Bloodhound.tokenizers.whitespace,
         local:[
           {
-            "courseid": "CPSC8700",
+            "courseid": "CPSC 8700",
             "fall": "fall-all",
             "spring": "spring-none",
             "coursename": "Introduction to Biometrics",
             "description": "XYZZYSPOON!",
-            "type": "Applications",
-            "tokens": [
-              "CPSC8700",
-              "Introduction to Biometrics",
-              "Applications"
-            ]
+            "type": "Applications"
           },
           {
-            "courseid": "CPSC8400",
+            "courseid": "CPSC 8400",
             "fall": "fall-none",
             "spring": "spring-all",
             "coursename": "Design and Analysis of Algorithms",
             "description": "XYZZYSPOON!",
-            "type": "Computing Foundations",
-            "tokens": [
-              "CPSC8400",
-              "Design and Analysis of Algorithms",
-              "Computing Foundations",
-              "Formal Thinking"
-            ]
+            "type": "Computing Foundations"
           },
           {
-            "courseid": "CPSC8620",
+            "courseid": "CPSC 8620",
             "fall": "fall-none",
             "spring": "spring-all",
             "coursename": "Database Management System Design",
             "description": "XYZZYSPOON!",
-            "type": "Applications",
-            "tokens": [
-              "CPSC8620",
-              "Database Management System Design",
-              "Applications", 
-              "Implementation"
-            ]
+            "type": "Applications"
           },
           {
-            "courseid": "CPSC8310",
+            "courseid": "CPSC 8310",
             "fall": "fall-all",
             "spring": "spring-none",
             "coursename": "Introduction to Human Centered Computing",
             "description": "XYZZYSPOON!",
-            "type": "Interactive Computing",
-            "tokens": [
-              "CPSC8310",
-              "Introduction to Human Centered Computing",
-              "Interactive Computing"
-            ]
+            "type": "Interactive Computing"
           },
           {
             "courseid": "CPSC 8100",
@@ -64,12 +42,7 @@ $(document).ready(function(){
             "spring": "spring-none",
             "coursename": "Introduction to Artificial Intelligence",
             "description": "XYZZYSPOON!",
-            "type": "Applications",
-            "tokens": [
-              "CPSC8100",
-              "Introduction to Artificial Intelligence",
-              "Applications"
-            ]
+            "type": "Applications"
           },
           {
             "courseid": "CPSC 6620",
@@ -77,12 +50,7 @@ $(document).ready(function(){
             "spring": "spring-all",
             "coursename": "Database Management Systems",
             "description": "XYZZYSPOON!",
-            "type": "Applications",
-            "tokens": [
-              "CPSC6620",
-              "Database Management Systems",
-              "Applications"
-            ]
+            "type": "Applications"
           },
             {
             "courseid": "CPSC 6620",
@@ -90,12 +58,7 @@ $(document).ready(function(){
             "spring": "spring-all",
             "coursename": "Database Management Systems",
             "description": "XYZZYSPOON!",
-            "type": "Applications",
-            "tokens": [
-              "CPSC6620",
-              "Database Management Systems",
-              "Applications"
-            ]
+            "type": "Applications"
           },
           ]
     });
@@ -109,5 +72,9 @@ $(document).ready(function(){
         name: 'cpscCourses',
         displayKey: 'coursename',
         source: courses.ttAdapter()
+    });
+    $('input.typeahead').on("typeahead:selected",function(obj, datum, name) {
+        $('.typeahead').typeahead('val',"");
+        console.log(datum);
     });
 });
