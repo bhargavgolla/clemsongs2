7a750e614d493190f994a0ed2eb78ad3f76d35e0
bhargavgolla <bhargav.golla@gmail.com>
2014-02-17 10:13:41 -0500

    Course selection is now visible on dashboard

--- js/app.js
+++ js/app.js
@@ -1,12 +1,30 @@
 var suggestionTemplateGen = function(obj){
     return '<p class="courseId">'+obj.courseid+'<br/>'+obj.type+'</p><p class="courseName">'+obj.coursename+'</p><p class="courseDescription">'+obj.description+'</p>';
 }
+var addedCourses = [];
+var addedCourseDatums = [];
+var addCourse = function(datum) {
+    console.log(datum);
+    if (addedCourses.indexOf(datum.courseid) == -1) {
+        addedCourses.push(datum.courseid);
+        addedCourseDatums.push(datum);
+        var course = '<div class="alert alert-warning alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>'+datum.courseid+':'+datum.coursename+'</strong></div>';
+        $('#conc'+datum.type+' div.panel-body').append(course);
+        $('#conc'+datum.type).collapse('show');
+    }
+}
 $(document).ready(function(){
     var option = "";
+    $("a.option").click(function(){
+        option = $(this).text();
+    });
     var courses = new Bloodhound({
 	    datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.courseid+" "+d.coursename+" "+d.type); },
 	    queryTokenizer: Bloodhound.tokenizers.whitespace,
-            prefetch: './assets/courses.json'
+            prefetch: {
+                url: './assets/courses.json',
+                ttl: 0
+            }
     });
     courses.initialize();
     $('.form-control').focus(function(){
@@ -27,6 +45,6 @@ $(document).ready(function(){
     });
     $('input.typeahead').on("typeahead:selected",function(obj, datum, name) {
         $('input.typeahead').typeahead('val',"");
-        console.log(datum);
+        addCourse(datum);
     });
 });
